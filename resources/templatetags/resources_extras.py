dcff72dfad448754be37ea0cb84336f652d769b0
phrrezende <phr.rezende@gmail.com>
2016-07-13 13:29:23 -0300

    estrutura tags e filtros

--- resources/templatetags/resources_extras.py
+++ resources/templatetags/resources_extras.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name="replace")
 def replace(value,arg):
     return value.replace(arg, "")
+
+#@register.filter(name="to_upper")
+#@stringfilter
+#def to_upper(value, arg):
+#    return "%s -%s " % (value.upper(), arg)
