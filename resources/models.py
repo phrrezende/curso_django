64b4a97b67af5ee814e1ec9928e23e1fe2a970ca
phrrezende <phr.rezende@gmail.com>
2016-07-15 09:35:35 -0300

    criacao da model vehicles

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Vehicle(models.Model):
+
+ 	name = models.CharField(max_length=30)
+ 	description= models.TextField(null=True, blank=True)
